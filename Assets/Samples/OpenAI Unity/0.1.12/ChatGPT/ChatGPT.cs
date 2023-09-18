dfdf011701b6599ff0942fa90f9c98b1d273d3ee
AUryeong <jykee7030@gmail.coma>
2023-09-18 20:33:34 +0900

    Add UniRx Assets and Add UniTask Asset and Finish Game

--- Assets/Samples/OpenAI Unity/0.1.12/ChatGPT/ChatGPT.cs
+++ Assets/Samples/OpenAI Unity/0.1.12/ChatGPT/ChatGPT.cs
@@ -5,7 +5,14 @@ using System.Collections.Generic;
 using ScrtipableObjects;
 using System.Net.Http;
 using System.Diagnostics;
+using UnityEngine.Networking;
+using Cysharp.Threading.Tasks;
+using UniRx;
+using System.Threading;
 using System;
+using System.IO;
+using System.Threading.Tasks;
+using Debug = UnityEngine.Debug;
 
 namespace OpenAI
 {
@@ -14,7 +21,7 @@ namespace OpenAI
         [SerializeField] private InputField inputField;
         [SerializeField] private Button button;
         [SerializeField] private ScrollRect scroll;
-        
+
         [SerializeField] private RectTransform sent;
         [SerializeField] private RectTransform received;
 
@@ -26,12 +33,24 @@ namespace OpenAI
         public bool isAssetLoading = false;
 
         private float height;
+
+        public string language;
+        
         private OpenAIApi openai = new OpenAIApi("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");
 
+        private const string API_KEY = "d41dabe1-8fc2-b223-be77-dfdba616e345:fx";
+        private const string ENDPOINT = "https://api-free.deepl.com/v2/translate?auth_key=";
+
+        public const string PATH = "D:/user/MoeGoe/output/output.wav";
+
         private List<ChatMessage> messages = new();
+        private CancellationToken token;
+        private HttpResponseMessage response;
 
         private void Start()
         {
+            token = this.GetCancellationTokenOnDestroy();
+
             audioSource = GetComponent<AudioSource>();
             if (isAssetLoading)
             {
@@ -49,6 +68,7 @@ namespace OpenAI
                     });
                 }
             }
+
             foreach (var prom in npc.prompt)
             {
                 messages.Add(new ChatMessage()
@@ -71,7 +91,7 @@ namespace OpenAI
                     Content = example.answer
                 });
             }
-            
+
             button.onClick.AddListener(SendReply);
 
             //try
@@ -95,54 +115,105 @@ namespace OpenAI
 
         private void Update()
         {
-            if(Input.GetKeyDown(KeyCode.Return))
+            if (Input.GetKeyDown(KeyCode.Return))
                 SendReply();
         }
 
         private async void AppendMessage(ChatMessage message)
         {
-            scroll.content.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 0);
+            if (message.Role == "user")
+            {
+                scroll.content.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 0);
 
-            var item = Instantiate(message.Role == "user" ? sent : received, scroll.content);
-            item.GetChild(0).GetChild(0).GetComponent<Text>().text = message.Content;
-            item.anchoredPosition = new Vector2(0, -height);
-            LayoutRebuilder.ForceRebuildLayoutImmediate(item);
-            height += item.sizeDelta.y;
-            scroll.content.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
-            scroll.verticalNormalizedPosition = 0;
+                var item = Instantiate(message.Role == "user" ? sent : received, scroll.content);
+                item.GetChild(0).GetChild(0).GetComponent<Text>().text = message.Content;
+                item.anchoredPosition = new Vector2(0, -height);
+                LayoutRebuilder.ForceRebuildLayoutImmediate(item);
+                height += item.sizeDelta.y;
+                scroll.content.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
+                scroll.verticalNormalizedPosition = 0;
+                return;
+            }
 
+            Debug.Log(message.Content);
+
+            string url = "http://127.0.0.1:5000/Text/" + message.Content.Replace("%20", " ");
+            if (language != "KO")
+            {
+                var result = GetTranslation(message.Content, token);
+                url = "http://127.0.0.1:5000/Text/" + await result;
+            }
             using var client = new HttpClient();
-            string url = "http://127.0.0.1:5000/ªªùªªì?ªÇª·ª¿¡£";
-            var response = await client.GetAsync(url);
+            StartCoroutine(DownloadTheAudio(message.Content));
+            response = await client.GetAsync(url);
             var content = await response.Content.ReadAsStringAsync();
-
-            StartCoroutine(DownloadTheAudio());
+            client.Dispose();
         }
 
-        IEnumerator DownloadTheAudio()
+        private async UniTask<string> GetTranslation(string speechText, CancellationToken ct)
         {
-            WWW www = new("file://" + Application.dataPath + "/MoeGoe/output/output.wav");
+            //POST«á«½«Ã«ÉªÎ«ê«¯«¨«¹«ÈªòíÂà÷
+            var requestInfo = ENDPOINT + API_KEY;
+            requestInfo += $"&text={speechText}&source_lang=KO&target_lang={language}";
+            var request = UnityWebRequest.Post(requestInfo, "Post");
+
+            //Ì¿Íýáôª±ö¢ªê
+            var second = TimeSpan.FromSeconds(3);
+            var result = await request.SendWebRequest().ToUniTask(cancellationToken: ct).Timeout(second);
+            var json = result.downloadHandler.text;
+            var data = JsonUtility.FromJson<TranslateData>(json);
+            Debug.Log(data.translations[0].text);
+            return data.translations[0].text;
+        }
+
+        IEnumerator DownloadTheAudio(string text)
+        {
+            while (!File.Exists(PATH))
+            {
+                yield return new WaitForFixedUpdate();
+            }
+
+            yield return new WaitForSeconds(3);
+            response?.Dispose();
+            
+            WWW www = new($"file://{PATH}");
             yield return www;
             AudioClip audioClip = www.GetAudioClip();
+            audioSource.clip = audioClip;
             audioSource.Play();
+            
+            File.Delete(PATH);
+
+            scroll.content.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 0);
+
+            var itemObj = Instantiate(received, scroll.content);
+            itemObj.GetChild(0).GetChild(0).GetComponent<Text>().text = text;
+            itemObj.anchoredPosition = new Vector2(0, -height);
+            LayoutRebuilder.ForceRebuildLayoutImmediate(itemObj);
+            height += itemObj.sizeDelta.y;
+            scroll.content.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
+            scroll.verticalNormalizedPosition = 0;
         }
 
         private async void SendReply()
         {
+            if (File.Exists(PATH))
+                File.Delete(PATH);
+
             var newMessage = new ChatMessage()
             {
                 Role = "user",
                 Content = inputField.text
             };
-            
+
             AppendMessage(newMessage);
-            
+
             messages.Add(newMessage);
-            
+
             button.enabled = false;
             inputField.text = "";
             inputField.enabled = false;
-            
+
             // Complete the instruction
             var completionResponse = await openai.CreateChatCompletion(new CreateChatCompletionRequest()
             {
@@ -154,7 +225,7 @@ namespace OpenAI
             {
                 var message = completionResponse.Choices[0].Message;
                 message.Content = message.Content.Trim();
-                
+
                 messages.Add(message);
                 AppendMessage(message);
             }
@@ -162,9 +233,22 @@ namespace OpenAI
             {
                 UnityEngine.Debug.LogWarning("No text was generated from this prompt.");
             }
-            
+
             button.enabled = true;
             inputField.enabled = true;
         }
+    }
+}
+
+[Serializable]
+public struct TranslateData
+{
+    public Translations[] translations;
+
+    [Serializable]
+    public struct Translations
+    {
+        public string detected_source_language;
+        public string text;
     }
 }
---
+++ Assets/WAV.cs
@@ -0,0 +1,115 @@
+﻿using System.IO;
+
+public class WAV
+	{
+		// Token: 0x060095AA RID: 38314 RVA: 0x002C22A0 File Offset: 0x002C04A0
+		private static float bytesToFloat(byte firstByte, byte secondByte)
+		{
+			short num = (short)((int)secondByte << 8 | (int)firstByte);
+			return (float)num / 32768f;
+		}
+
+		// Token: 0x060095AB RID: 38315 RVA: 0x002C22C4 File Offset: 0x002C04C4
+		private static int bytesToInt(byte[] bytes, int offset = 0)
+		{
+			int num = 0;
+			for (int i = 0; i < 4; i++)
+			{
+				num |= (int)bytes[offset + i] << i * 8;
+			}
+			return num;
+		}
+
+		// Token: 0x060095AC RID: 38316 RVA: 0x002C22FC File Offset: 0x002C04FC
+		private static byte[] GetBytes(string filename)
+		{
+			return File.ReadAllBytes(filename);
+		}
+
+		// Token: 0x1700105B RID: 4187
+		// (get) Token: 0x060095AD RID: 38317 RVA: 0x002C2314 File Offset: 0x002C0514
+		// (set) Token: 0x060095AE RID: 38318 RVA: 0x002C231C File Offset: 0x002C051C
+		public float[] LeftChannel { get; internal set; }
+
+		// Token: 0x1700105C RID: 4188
+		// (get) Token: 0x060095AF RID: 38319 RVA: 0x002C2328 File Offset: 0x002C0528
+		// (set) Token: 0x060095B0 RID: 38320 RVA: 0x002C2330 File Offset: 0x002C0530
+		public float[] RightChannel { get; internal set; }
+
+		// Token: 0x1700105D RID: 4189
+		// (get) Token: 0x060095B1 RID: 38321 RVA: 0x002C233C File Offset: 0x002C053C
+		// (set) Token: 0x060095B2 RID: 38322 RVA: 0x002C2344 File Offset: 0x002C0544
+		public int ChannelCount { get; internal set; }
+
+		// Token: 0x1700105E RID: 4190
+		// (get) Token: 0x060095B3 RID: 38323 RVA: 0x002C2350 File Offset: 0x002C0550
+		// (set) Token: 0x060095B4 RID: 38324 RVA: 0x002C2358 File Offset: 0x002C0558
+		public int SampleCount { get; internal set; }
+
+		// Token: 0x1700105F RID: 4191
+		// (get) Token: 0x060095B5 RID: 38325 RVA: 0x002C2364 File Offset: 0x002C0564
+		// (set) Token: 0x060095B6 RID: 38326 RVA: 0x002C236C File Offset: 0x002C056C
+		public int Frequency { get; internal set; }
+
+		// Token: 0x060095B7 RID: 38327 RVA: 0x002C2378 File Offset: 0x002C0578
+		public WAV(string filename) : this(WAV.GetBytes(filename))
+		{
+		}
+
+		// Token: 0x060095B8 RID: 38328 RVA: 0x002C2388 File Offset: 0x002C0588
+		public WAV(byte[] wav)
+		{
+			this.ChannelCount = (int)wav[22];
+			this.Frequency = WAV.bytesToInt(wav, 24);
+			int i = 12;
+			while (wav[i] != 100 || wav[i + 1] != 97 || wav[i + 2] != 116 || wav[i + 3] != 97)
+			{
+				i += 4;
+				int num = (int)wav[i] + (int)wav[i + 1] * 256 + (int)wav[i + 2] * 65536 + (int)wav[i + 3] * 16777216;
+				i += 4 + num;
+			}
+			i += 8;
+			this.SampleCount = (wav.Length - i) / 2;
+			bool flag = this.ChannelCount == 2;
+			if (flag)
+			{
+				this.SampleCount /= 2;
+			}
+			this.LeftChannel = new float[this.SampleCount];
+			bool flag2 = this.ChannelCount == 2;
+			if (flag2)
+			{
+				this.RightChannel = new float[this.SampleCount];
+			}
+			else
+			{
+				this.RightChannel = null;
+			}
+			int num2 = 0;
+			while (i < wav.Length)
+			{
+				this.LeftChannel[num2] = WAV.bytesToFloat(wav[i], wav[i + 1]);
+				i += 2;
+				bool flag3 = this.ChannelCount == 2;
+				if (flag3)
+				{
+					this.RightChannel[num2] = WAV.bytesToFloat(wav[i], wav[i + 1]);
+					i += 2;
+				}
+				num2++;
+			}
+		}
+
+		// Token: 0x060095B9 RID: 38329 RVA: 0x002C24E4 File Offset: 0x002C06E4
+		public override string ToString()
+		{
+			return string.Format("[WAV: LeftChannel={0}, RightChannel={1}, ChannelCount={2}, SampleCount={3}, Frequency={4}]", new object[]
+			{
+				this.LeftChannel,
+				this.RightChannel,
+				this.ChannelCount,
+				this.SampleCount,
+				this.Frequency
+			});
+		}
+	}
